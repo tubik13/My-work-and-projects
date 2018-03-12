ce7d8d955efa2e70763536decb7a69207c7cf9a6
Alexey <tubiktubik13@gmail.com>
2018-03-12 17:27:55 +0300

    questbook

---
+++ project/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Старт</title>
+<link rel="stylesheet" type="text/css" href="/css/gb.css">
+</head>
+<body>
+	<p><a href="guestbook.php">Перейти на гостевую книгу</a></p>
+	<p><a href="application/model/generator.php">Сгенерировать 70 случайных сообщений (только с локального хоста)</a></p>
+</body>
+</html>
